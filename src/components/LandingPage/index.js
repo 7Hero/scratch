55a3e338aeb162d930fc44029e2ec70b111f2dee
7Hero <vpoopescu07@gmail.com>
2022-03-21 11:34:30 +0200

    update

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import logo from '../../logo.svg'
+import { Container } from '@mui/material'
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <>
+    <Container sx= {{ mx: 12 }}>
+      <img style= {{marginTop:'72px'}}src={logo}/>
+      <div style= {{marginTop:'100px'}}>
+          <p>Vasea</p>
+          <p>Text mic</p>
+      </div>
+    </Container>
+    </>
   )
 }
 
